64b2c796562f3779a9b4d57749180ee5bc791e31
TheChupa <160878340+TheChupa@users.noreply.github.com>
2025-06-03 13:18:18 -0500

    deleted folders

--- hello-world/src/main/java/org/example/Main.java
+++ hello-world/src/main/java/org/example/Main.java
@@ -1,18 +1,17 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
-        System.out.println("Hello and welcome!");
-        System.out.println("Hello and welcome!");
-        System.out.println("Hello and welcome!");
-        System.out.println("Hello and welcome!");
 
+        String exampleString = " Rutabaga ";
+        String trimmed = exampleString.trim();
+        String[] splits = exampleString.split("a");
+        for (String split : splits) {
+            System.out.println(split);
 
         }
-    
+        System.out.println(trimmed);
+
+    }
 }
